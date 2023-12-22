c7b4027c73e660c94e1c90b6d88e9c95600de173
Nematjon100 <nematdev@gmail.com>
2023-12-22 10:47:01 +0500

    Add Url-shortener-result click styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
+const elUrlShortener = document.querySelector('.url-shortener')
 const elUrlShortenerForm = document.querySelector('.js-url-shortener-form');
 const elUrlShortenerResults =document.querySelector('.js-url-shortener__results');
+const elCopyButton = document.querySelector('.js-copy-button');
 
-elUrlShortenerForm.addEventListener('submit', function (evt) {
-  evt.preventDefault ();
-  elUrlShortenerResults.classList.add('url-shortener__results--open');
-})
+
+if (elUrlShortenerForm) {
+  elUrlShortenerForm.addEventListener('submit', function (evt) {
+    evt.preventDefault ();
+
+    elUrlShortenerResults.classList.add('url-shortener__results--open');
+  })
+}
+
+if (elUrlShortener) {
+  elUrlShortener.addEventListener('click', function (evt) {
+    if (evt.target.matches('.js-copy-button')) {
+      evt.target.textContent = 'Copied!';
+      evt.target.classList.add('copy-button--copied');
+
+      setTimeout(function () {
+        evt.target.textContent = 'Copy';
+        evt.target.classList.remove('copy-button--copied');
+      }, 2000)
+    }
+  })
+}
